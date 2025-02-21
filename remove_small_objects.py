6706754d278d383bc57fe665f6aa5608fbf03322
Joel Cranmer <42.joel.cranmer@gmail.com>
2025-02-21 08:47:19 -0700

    Update remove_small_objects.py
    Clean up arguments and test with Inkscape v1.2

--- remove_small_objects.py
+++ remove_small_objects.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python3
 import inkex
 from lxml import etree
 
 class RemoveSmallObjects(inkex.EffectExtension):
+    """Deletes small objects that may exist in your Inkscape document."""
     def add_arguments(self, pars):
-        pars.add_argument("--width_threshold", type=float, default=10.0, help="Width Threshold")
-        pars.add_argument("--height_threshold", type=float, default=10.0, help="Height Threshold")
-        pars.add_argument("--lock_ratio", type=inkex.Boolean, default=False, help="Lock Width and Height")
+        pars.add_argument(
+            "--width_threshold",
+            type=float,
+            default=0.50,
+            help="Width Threshold",
+        )
+        pars.add_argument(
+            "--height_threshold",
+            type=float,
+            default=0.50,
+            help="Height Threshold",
+        )
 
     def effect(self):
         width_threshold = self.options.width_threshold
         height_threshold = self.options.height_threshold
-        lock_ratio = self.options.lock_ratio
-
-        if lock_ratio:
-            height_threshold = width_threshold
 
         for element in self.svg.descendants().filter(inkex.PathElement):
             if element.is_visible():
